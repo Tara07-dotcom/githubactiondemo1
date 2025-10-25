196ab4aa021fa1f03a2acbf63051c778bd232e13
unknown <taraprasadtripathy82@gmail.com>
2025-10-25 16:52:00 +0530

    testing1

--- read_data_api.py
+++ read_data_api.py
@@ -1,6 +1,6 @@
 import requests 
 import pandas as pd
-response=requests.get("https //jsonplaceholder.typicode.come/users")
+response=requests.get("https://jsonplaceholder.typicode.come/users")
 data=response.json()
 df=pd.DataFrame(data)
 print(df)
